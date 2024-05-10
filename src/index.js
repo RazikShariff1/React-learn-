f1549c545adef6b5159b0a92a46e653c9add9896
razikShariff <razik.shariff234@gmail.com>
2024-05-10 22:42:31 +0530

    learn-1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let date=new Date().toLocaleDateString()
+// let name="Razik"
+// ReactDOM.render(<><h1>hello {name}</h1>
+// <p>this is the secound tag</p></>,document.getElementById("root"))
+function img(){
+let img=" https://picsum.photos/400/300"
+    document.getElementById("img").src=img
+}
+setInterval(img, 1000);//not working use api for the the same later
+ReactDOM.render(<><h1 id='heading' /*contentEditable  //try this attribute*/>Secound app</h1>
+<p>{`todays date is ${date}`}</p>
+<img id='img' src='' alt="randomimg"></img></>,document.getElementById("root"))
